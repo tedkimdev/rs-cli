3d7436be445a7391f378aea78acd7e02126f1cc6
ted <dev.ted.kim@gmail.com>
2025-08-02 23:23:04 -0400

    add models

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod models;
+
+use std::collections::HashMap;
+
+use models::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut db = DBState {
+        last_item_id: 0,
+        epics: HashMap::new(),
+        stories: HashMap::new(),
+    };
+
+    let epic = Epic::new("project 1".to_string(), "This is project 1".to_string());
+    let story = Story::new("story 1".to_string(), "This is story 1".to_string());
+
+    db.epics.insert(epic.id, epic);
+    db.stories.insert(story.id, story);
+
+    println!("{:?}", db);
 }
--- src/models.rs
+++ src/models.rs
@@ -1,28 +1,56 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
 pub enum Status {
-    // TODO: add fields (make sure the fields are public)
+    Open,
+    InProgress,
+    Resoved,
+    Closed,
 }
 
+#[derive(Debug)]
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
+    pub id: u32,
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        Self {
+            id: 0, // TODO: use global integer id counter
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![],
+        }
     }
 }
 
+#[derive(Debug)]
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    pub id: u32,
+    pub name: String,
+    pub description: String,
+    pub status: Status,
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        Self {
+            id: 0, // TODO: use global integer id counter
+            name,
+            description,
+            status: Status::Open,
+        }
     }
 }
 
+#[derive(Debug)]
 pub struct DBState {
-    // This struct represents the entire db state which includes the last_item_id, epics, and stories
-    // TODO: add fields (make sure the fields are public)
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
 }
